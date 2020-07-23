c09e4def2f8ef2b95a2c7e459fc79f580a2ff8e9
Will Forsberg <forsbergw82@gmail.com>
2020-07-23 11:06:24 -0500

    fix blocks.h

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
-        {"", "~/bin/statusbar/volume",                                          0,              10},
+        {"", "~/.local/bin/volume",                                          0,              10},
 
-        {"", "~/bin/statusbar/battery",                                         5,              0},
+        {"", "/.local/bin/battery",                                         5,              0},
 
-        {"", "~/bin/statusbar/clock",                                           5,              0},
+        {"", "/.local/bin/clock",                                           5,              0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 mkdir -p .local/bin
 cp bin/* .local/bin
+rm -R bin
