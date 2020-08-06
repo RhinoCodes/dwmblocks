5578a8f555a1cd10fbd409d26845428b364ad930
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-08-06 17:54:42 +0000

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -1,11 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/        /*Command*/             /*Update Interval*/     /*Update Signal*/
-        {"", "~/.local/bin/volume",                                          0,              10},
+        {"", "/.local/bin/battery",               5,                      0},
 
-        {"", "/.local/bin/battery",                                         5,              0},
-
-        {"", "/.local/bin/clock",                                           5,              0},
+        {"", "/.local/bin/clock",                 5,                      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
